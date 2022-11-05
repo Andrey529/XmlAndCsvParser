a908bcbea06b316064c1097f8deaaac878c2cd38
Andrey529 <andr02eyes@gmail.com>
2022-11-06 01:04:23 +0300

    done xml parser

--- XmlParser.h
+++ XmlParser.h
@@ -1,20 +1,13 @@
 #ifndef XMLANDCSVPARSER_XMLPARSER_H
 #define XMLANDCSVPARSER_XMLPARSER_H
 
-//#include <boost/property_tree/ptree.hpp>
-//#include <boost/property_tree/xml_parser.hpp>
 #include <string>
 #include <set>
 #include <map>
 #include <iostream>
 #include "Address.h"
 #include <fstream>
-
-//#include <xercesc/util/PlatformUtils.hpp>
-//#include <xercesc/sax2/SAX2XMLReader.hpp>
-//#include <xercesc/sax2/XMLReaderFactory.hpp>
-//#include <xercesc/sax2/DefaultHandler.hpp>
-//#include <xercesc/util/XMLString.hpp>
+#include <locale>
 
 class XmlParser {
 private:
@@ -25,10 +18,8 @@ private:
 public:
     XmlParser() = default;
     void Read(const std::string &filePath); // read xml file and fill
-
     const std::string &getFilePath() const { return filePath_; }
     std::string &getFilePath() { return filePath_; }
-
     void writeDuplicateRecords();
     void writeCountBuildingsForEachCity();
 };
@@ -37,12 +28,10 @@ void XmlParser::Read(const std::string &filePath) {
 
     filePath_ = filePath;
 
-    setlocale(LC_ALL,"ru_RU.UTF-8");
     std::wstring line;
     std::wifstream inFile;
     inFile.open(filePath_);
     inFile.imbue(std::locale("ru_RU.UTF8"));
-
 
     if (inFile.is_open()) {
         while (!inFile.eof()) {
@@ -94,19 +83,16 @@ void XmlParser::Read(const std::string &filePath) {
                     auto iteratorToNewRecord = duplicateRecords_.find(address);
                     if (iteratorToNewRecord == duplicateRecords_.end()) { // if this record is new in duplicate records
                         duplicateRecords_.insert(std::pair(address, 1));
-                    }
-                    else { // increment the count of duplicate record
+                    } else { // increment the count of duplicate record
                         iteratorToNewRecord->second++;
                     }
-                }
-                else {
+                } else {
                     auto iteratorToCity = countBuildingsForEachCity_.find(address.city_);
                     if (iteratorToCity == countBuildingsForEachCity_.end()) { // if this city is new
-                        std::array<unsigned int, 5> tmp = {0,0,0,0,0};
+                        std::array<unsigned int, 5> tmp = {0, 0, 0, 0, 0};
                         tmp[std::stoi(address.floor_) - 1] = 1;
                         countBuildingsForEachCity_.insert(std::pair(address.city_, tmp));
-                    }
-                    else {
+                    } else {
                         iteratorToCity->second[std::stoi(address.floor_) - 1]++;
                     }
                 }
@@ -114,124 +100,51 @@ void XmlParser::Read(const std::string &filePath) {
         }
 
         inFile.close();
+    } else {
+        std::cout << "Unable to open input file " << filePath_ << std::endl;
     }
-    else {
-        std::cout << "Unable to open file " << filePath_ << std::endl;
-    }
-
-
-
-//    using namespace xercesc;
-//
-//    try {
-//        XMLPlatformUtils::Initialize();
-//    }
-//    catch (const XMLException& toCatch) {
-//        std::cout << "Unable to initialize xml parser" << std::endl;
-//        std::cout << XMLString::transcode(toCatch.getMessage());
-//        // Do failure processing here
-//        return;
-//    }
-//
-//    SAX2XMLReader *parser = XMLReaderFactory::createXMLReader();
-//    parser->setFeature(XMLUni::fgSAX2CoreValidation, true);
-//    parser->setFeature(XMLUni::fgSAX2CoreNameSpaces, true);   // optional
-//
-//    auto* defaultHandler = new DefaultHandler();
-//    parser->setContentHandler(defaultHandler);
-//    parser->setErrorHandler(defaultHandler);
-//
-//    try {
-//        parser->parse(filePath_.c_str());
-//    }
-//    catch (const XMLException& toCatch) {
-//        char* message = XMLString::transcode(toCatch.getMessage());
-//        std::cout << "Exception message is: \n"
-//             << message << "\n";
-//        XMLString::release(&message);
-//        return;
-//    }
-//    catch (const SAXParseException& toCatch) {
-//        char* message = XMLString::transcode(toCatch.getMessage());
-//        std::cout << "Exception message is: \n"
-//             << message << "\n";
-//        XMLString::release(&message);
-//        return;
-//    }
-//    catch (...) {
-//        std::cout << "Unexpected Exception \n" ;
-//        return;
-//    }
-//
-//    delete parser;
-//    delete defaultHandler;
-//
-//    XMLPlatformUtils::Terminate();
-
-
-
-
-//    boost::property_tree::ptree tree;
-//    boost::property_tree::read_xml(filePath_, tree);
-
-
-//    for (boost::property_tree::ptree::value_type &v : tree.get_child("root")) {
-//        const boost::property_tree::ptree &attributes = v.second.get_child("<xmlattr>");
-//        Address address(std::move(attributes.get<std::string>("city")),
-//                        std::move(attributes.get<std::string>("street")),
-//                        std::move(attributes.get<std::string>("house")),
-//                        std::move(attributes.get<std::string>("floor"))
-//                        );
-//
-//        unsigned long sizeBeforeInsertion = allRecords_.size();
-//        allRecords_.insert(address);
-//        if (sizeBeforeInsertion == allRecords_.size()) { // record is duplicate
-//            auto iteratorToNewRecord = duplicateRecords_.find(address);
-//            if (iteratorToNewRecord == duplicateRecords_.end()) { // if this record is new in duplicate records
-//                duplicateRecords_.insert(std::pair(address, 1));
-//            }
-//            else { // increment the count of duplicate record
-//                iteratorToNewRecord->second++;
-//            }
-//        }
-//        else {
-//            auto iteratorToCity = countBuildingsForEachCity_.find(address.city_);
-//            if (iteratorToCity == countBuildingsForEachCity_.end()) { // if this city is new
-//                std::array<unsigned int, 5> tmp = {0,0,0,0,0};
-//                tmp[std::stoi(address.floor_) - 1] = 1;
-//                countBuildingsForEachCity_.insert(std::pair(address.city_, tmp));
-//            }
-//            else {
-//                iteratorToCity->second[std::stoi(address.floor_) - 1]++;
-//            }
-//        }
-//    }
-
-//    std::cout << "In file " << filePath_ << " " << allRecords_.size() << " unique records." << std::endl;
 }
 
 void XmlParser::writeDuplicateRecords() {
     if (!duplicateRecords_.empty()) {
-        ::setlocale( LC_ALL , "russian_russia.866" );
-        std::wcout.imbue(std::locale("ru_RU.UTF-8"));
-
-        std::wcout << "\nDuplicate records:" << std::endl;
-        for (const auto& [record, count] : duplicateRecords_) {
-            std::wcout.imbue(std::locale("ru_RU.UTF8"));
-            std::wcout << "city = " << record.city_ << ", street = " << record.street_ << ", house = " << record.house_
-                      << ", floor = " << record.floor_ << " - occurs " << count << " times." << std::endl;
+        std::wofstream outFile("/home/andrey/Projects/XmlAndCsvParser/data/output.txt", std::ios::app);
+        if (outFile.is_open()) {
+            outFile.imbue(std::locale("ru_RU.UTF-8"));
+//            std::wcout.imbue(std::locale("ru_RU.UTF-8"));
+//            std::wcout << "\nDuplicate records:" << std::endl;
+            outFile << "\nDuplicate records:" << std::endl;
+            for (const auto &[record, count]: duplicateRecords_) {
+//                std::wcout << "city = " << record.city_ << ", street = " << record.street_ << ", house = " << record.house_
+//                           << ", floor = " << record.floor_ << " - occurs " << count << " times." << std::endl;
+                outFile << "city = " << record.city_ << ", street = " << record.street_ << ", house = " << record.house_
+                        << ", floor = " << record.floor_ << " - occurs " << count << " times." << std::endl;
+            }
+            outFile.close();
+        } else {
+            std::wcout << "Unable to open output file" << std::endl;
         }
     }
 }
 
 void XmlParser::writeCountBuildingsForEachCity() {
     if (!countBuildingsForEachCity_.empty()) {
-        std::wcout << "\nCount buildings for each city:" << std::endl;
-        for (const auto& [city, building] : countBuildingsForEachCity_) {
-            std::wcout << "In the city " << city << ":" << std::endl;
-            for (int i = 0; i < 5; ++i) {
-                std::wcout << "\t" << building[i] << " " << i+1 << " floor houses" << std::endl;
+        std::wofstream outFile("/home/andrey/Projects/XmlAndCsvParser/data/output.txt", std::ios::app);
+        if (outFile.is_open()) {
+            outFile.imbue(std::locale("ru_RU.UTF-8"));
+//            std::wcout.imbue(std::locale("ru_RU.UTF-8"));
+//            std::wcout << "\nCount buildings for each city:" << std::endl;
+            outFile << "\nCount buildings for each city:" << std::endl;
+            for (const auto &[city, building]: countBuildingsForEachCity_) {
+//                std::wcout << "In the city " << city << ":" << std::endl;
+                outFile << "In the city " << city << ":" << std::endl;
+                for (int i = 0; i < 5; ++i) {
+//                    std::wcout << "\t" << building[i] << " " << i + 1 << " floor houses" << std::endl;
+                    outFile << "\t" << building[i] << " " << i + 1 << " floor houses" << std::endl;
+                }
             }
+            outFile.close();
+        } else {
+            std::wcout << "Unable to open output file" << std::endl;
         }
     }
 }
--- main.cpp
+++ main.cpp
@@ -3,9 +3,6 @@
 
 int main() {
 
-    setlocale(LC_ALL, "ru_RU.UTF-8");
-    std::wcout.imbue(std::locale("ru_RU.UTF-8"));
-
     XmlParser parser;
     std::string filename;
     std::cout << "Input file path" << std::endl;
@@ -14,13 +11,6 @@ int main() {
     parser.Read("/home/andrey/Projects/XmlAndCsvParser/data/" + filename);
     parser.writeDuplicateRecords();
     parser.writeCountBuildingsForEachCity();
-
-//    std::wstring wstring(L"оооооох эта локаль");
-//    std::wcout << L"абракадабра танцы с бубном" << std::endl;
-//    std::wcout << wstring << std::endl;
-
-
-
 
     return 0;
 }
